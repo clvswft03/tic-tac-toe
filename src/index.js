5d4719acd004997fb78dfe64fa1074cc48fa7e07
손수민 <sohn0521@gmail.com>
2019-07-08 16:52:49 +0900

    Init project

--- src/index.js
+++ src/index.js
@@ -5,9 +5,55 @@ import "./styles.css";
 
 class Game extends React.Component {
   render() {
-    return <div className="game">TEST</div>;
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
   }
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Game />, rootElement);
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    return <button className="square">{/* TODO */}</button>;
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
